39b60567f8a24ceb07bbfbf2a54783f2bdf72247
b.semeniuk <bohdansem14@gmail.com>
2023-09-14 20:13:54 +0300

    added next pictures, tries and small logo

--- src/js/game.js
+++ src/js/game.js
@@ -1,20 +1,59 @@
-import { WORDS_LIST, KEYBOARD_LETTERS} from "./constant";
+import { WORDS_LIST, KEYBOARD_LETTERS } from "./constant";
 
-const divGame = document.getElementById('game')
+const divGame = document.getElementById("game");
+const logoH1 = document.getElementById("logo");
 
 const createPlaceholder = () => {
-   const word = sessionStorage.getItem("word");
-   const wordArr = Array.from(word)
-    const placeholderLetters = wordArr.reduce((acc, curr, i) => {
-        return acc + `<h1 class="letter" id="letter_${i}">_</h1>`
-    }, "")
-   return `<div id="placeholders" class="placeholder-wrapper">${placeholderLetters}</div>`
-}
+  const word = sessionStorage.getItem("word");
+  const wordArr = Array.from(word);
+  const placeholderLetters = wordArr.reduce((acc, curr, i) => {
+    return acc + `<h1 class="letter" id="letter_${i}">_</h1>`;
+  }, "");
+  return `<div id="placeholders" class="placeholder-wrapper">${placeholderLetters}</div>`;
+};
+
+export const createKeyboard = () => {
+  const keyboard = document.createElement("div");
+  keyboard.classList.add("keyboard");
+  keyboard.id = "keyboard";
+
+  const keyboardButtons = KEYBOARD_LETTERS.reduce((acc, curr) => {
+    return (
+      acc +
+      `<button class="button-primary keyboard-button" id="${curr}">${curr}</button>`
+    );
+  }, "");
+
+  keyboard.innerHTML = keyboardButtons;
+  return keyboard;
+};
+
+export const createImage = () => {
+  const image = document.createElement("img");
+  image.classList.add("hangman-img");
+  image.src = "images/hg-10.png";
+  image.alt = "hangman image";
+  image.id = "hangman-img";
+
+  return image;
+};
 
 export const startGame = () => {
-    const wordIndex = Math.round(Math.random() * WORDS_LIST.length)
-    const wordToGuess = WORDS_LIST[wordIndex]
-    sessionStorage.setItem("word", wordToGuess)
+  logoH1.classList.add("logo-sm");
 
-    divGame.innerHTML = createPlaceholder()
-}
+  const wordIndex = Math.round(Math.random() * WORDS_LIST.length);
+  const wordToGuess = WORDS_LIST[wordIndex];
+  sessionStorage.setItem("word", wordToGuess);
+
+  divGame.innerHTML = createPlaceholder();
+  divGame.innerHTML += `<p id="tries" class="mt-2">TRIES LEFT: <span id="tries-left" class="font-medium text-red-600">10</span> </p>`;
+
+  const keyboardDiv = createKeyboard();
+  keyboardDiv.addEventListener("click", (e) => {
+    console.log(e.target.id);
+  });
+
+  divGame.appendChild(keyboardDiv);
+  const imageImg = createImage();
+  divGame.prepend(imageImg);
+};
